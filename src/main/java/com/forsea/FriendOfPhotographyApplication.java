e83327b27fb16b50d162dba88b91585d7ee327d2
for-sea <2595140734@qq.com>
2022-05-08 22:46:34 +0800

    创建项目

---
+++ src/main/java/com/forsea/FriendOfPhotographyApplication.java
@@ -0,0 +1,13 @@
+package com.forsea;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendOfPhotographyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendOfPhotographyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/forsea/friendofphotography/FriendOfPhotographyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forsea.friendofphotography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendOfPhotographyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
